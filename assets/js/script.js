e8d98f0735242ebb977a05c126980d5bbc3e0836
dltorrise <dltorrise@gmail.com>
2023-01-15 02:18:48 -0600

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ var previousVideoResults = document.getElementById("previous-results")
 var gifInVideo = document.getElementById("gif-for-video")
 var backgroundSound = document.getElementById("youtube-video")
 
-var videoErrorMessage = document.getElementById("video-error")
+//var videoErrorMessage = document.getElementById("video-error")
 
 
 //variables
@@ -131,28 +131,25 @@ function getVideo(search) {
 
  var gifClickHandler = function (event) { //only purpose of this is to define variable for gif
     console.log("Gif clicked")
-    if (pickedGif) {
-        pickedGif = '' //clears out if someone already chose one
-    }
+    pickedGif = ''
+    gifInVideo.removeAttribute('src')
     pickedGif = event.target.getAttribute('data-gif');
     console.log(pickedGif)
-    if (pickedVideo) {
-        renderVideo(pickedGif, pickedVideo) //runs other function so long as both variables are defined
-    }
+    gifInVideo.src = pickedGif 
 }
 
   var pickedVideo
 
   var videoClickHandler = function (event) { //only purpose of this is to define pickedVideo
     console.log("Video clicked")
-    // if (pickedVideo) {
-    //     pickedVideo = '' //clears out if someone already chose one
-    // }
+    pickedVideo = '' //clears out variable
+    backgroundSound.removeAttribute('src') //when you click it a second time
+    console.log(backgroundSound.src)
     pickedVideo = event.target.getAttribute('data-video'); //embed key of video you pick
     console.log(pickedVideo)
-    if (pickedGif) {
-        renderVideo(pickedGif, pickedVideo) //runs other function so long as both variables are defined
+    backgroundSound.src = `https://www.youtube.com/embed/${pickedVideo}?enablejsapi=1&version=3&playerapiid=ytplayer` 
     }
+
     //embeddable might be deprecated, actually not entirely certain why this code isn't working
     // if (isEmbeddable(pickedVideo) && (pickedGif)) { //runs embedkey into function
     //         console.log("working")
@@ -161,22 +158,7 @@ function getVideo(search) {
     //     videoErrorMessage.textContent = "Sorry, this video is not embeddable. Please choose another one"
     //     videoErrorMessage.classList.add("red") //text color red
     // }
-    
-  };
 
-//create a promise that adds the autoplay and mute on there and then unmutes itself
-//puts the gif on top again
-
-//create another function that takes parameters pickedGif and pickedVideo, which will render video
-function renderVideo(pickedGif, pickedVideo) {
-    console.log("function runs")
-    //render gif
-    gifInVideo.src = pickedGif //value of attribute, url of gif
-    console.log(gifInVideo.src)
-    //render video
-    backgroundSound.src = `https://www.youtube.com/embed/${pickedVideo}?enablejsapi=1&version=3&playerapiid=ytplayer` 
-    console.log(backgroundSound.src)
-}
 
 //function to play video
 
@@ -227,6 +209,8 @@ function gifSearchSubmit(event) {
 // create video local storage 
 function createVideo() {
     //but at some point we also need to push chosen value onto these arrays in other functions
+    pastVideoPicks.push(pickedVideo)
+    pastGifPicks.push(pickedGif)
     localStorage.setItem("videoPicks", JSON.stringify(pastVideoPicks))
     localStorage.setItem("gifPicks", JSON.stringify(pastGifPicks))
 }
@@ -234,21 +218,27 @@ function createVideo() {
 //previous video button storage
 function previousVideo() {
     previousVideoResults.textContent = "Last 5 Videos"
-    previousVideoResults.classList.add("h5", ".text-primary")
+    console.log("previous videos clicked")
+    //previousVideoResults.classList.add("h5", ".text-primary")
     var listOfVideos = document.createElement('ul') //creates box for list
     previousVideoResults.appendChild(listOfVideos) //appends it to search container
-    for (i=0; i<5; i++) { //actually doesn't matter which array we use bc they should store same amount
+    for (i=0; i<pastGifPicks.length; i++) { //actually doesn't matter which array we use bc they should store same amount
         if (pastGifPicks.length>5) {
           pastGifPicks.shift() //removes first element  
           pastVideoPicks.shift()
         }
         var pastVideo = document.createElement('li')
-        pastVideo.classList.add("list-group-item")
-        pastVideo.addEventListener("click", buttonClickHandler)
-        pastVideo.textContent = histoire[i]
-        pastVideo.setAttribute('data-city', histoire[i])
-        console.log(pastVideo.getAttribute('data-city'))
-        listOfCities.appendChild(pastVideo)
+        var pastVideoThumbnail = document.createElement('img')
+        //pastVideo.classList.add("list-group-item")
+        pastVideo.addEventListener("click", gifClickHandler) //so you can click on it
+        pastVideo.setAttribute('data-video', pastVideoPicks[i])
+        pastVideoThumbnail.src = ///how am I going to do this without another fetch?
+        console.log(pastVideo.getAttribute('data-video'))
+        listOfVideos.appendChild(pastVideo)
+        var pastGif = document.createElement('li')
+        pastGif.addEventListener("click", videoClickHandler)
+        pastGif.setAttribute('data-gif', pastGifPicks[i])
+        listOfVideos.appendChild(pastGif) //this should put them next to each other
     }
 }
 
@@ -263,11 +253,11 @@ getGifBtn.addEventListener("submit", gifSearchSubmit)
 
 //event listener for create video button
 
-getCreateVideoBtn.addEventListener("submit",createVideo)
+getCreateVideoBtn.addEventListener("click",createVideo)
 
 //event listener for previous videos button
 
-getPreviousVideoBtn.addEventListener("submit",previousVideo)
+getPreviousVideoBtn.addEventListener("click",previousVideo)
 
 // put an attribute on search results and then do event.target to determine
 // which one, look at 6.21
